274e99bd7f66a6da53fabdc75990f61b9491d9bf
CAVID SAATOV <77391057+SaatovCavid@users.noreply.github.com>
2021-02-18 23:19:11 -0800

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('button').click(function(){
-        $('.header-content').fadeOut('slow', function(){
-            $('.header-content').fadeIn(3000);
-        });
-    })
-})
